c556cb93d6c4abee08b01e0e6ac02dad1c0f8329
Aditya Choudhary <124274939+adiitya24@users.noreply.github.com>
2023-07-25 18:34:34 +0530

    Add files via upload

---
+++ project3.js
@@ -0,0 +1,79 @@
+console.log("Welcome to Tic Tac Toe")
+let music=new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover=new Audio("gameover.mp3")
+let turn="x"
+let over=false;
+
+//function to change rhe turn
+const changeTurn=()=>{
+    return turn==="x"?"0":"x"
+}
+
+//function to check for a win
+const checkWin=()=>{
+    let boxtext=document.getElementsByClassName('boxtext');
+
+let wins=[
+    [0,1,2]
+    ,[3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+wins.forEach(e=>{
+if((boxtext[e[0]].innerText===boxtext[e[1]].innerText)&&(boxtext[e[1]].innerText===boxtext[e[2]].innerText)&&(boxtext[e[0]].innerText!==""))
+{
+    document.querySelector('.info').innerText=boxtext[e[0]].innerText+" Won";
+over=true;
+gameover.play();
+
+setInterval(music.play(),2000);
+let set=setTimeout(()=>{
+  
+
+},3000);
+//clearTimeout(set);
+
+
+
+document.querySelector('.img-box').getElementsByTagName('img')[0].style.width="200px"
+}
+})
+}
+
+//game logic
+//music.play();
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext=element.querySelector('.boxtext');
+    element.addEventListener('click',()=>{
+     if(boxtext.innerText==''){
+        boxtext.innerText=turn;
+      turn= changeTurn();
+        audioTurn.play();
+        checkWin();
+        if(!over){  
+             document.getElementsByClassName("info")[0].innerText="Turn for "+turn;
+    }
+       }   
+    })
+})
+
+//add onclick listner to reset button
+reset.addEventListener('click',()=>{
+let boxtexts=document.querySelectorAll('.boxtext');
+Array.from(boxtexts).forEach(element=>{
+    element.innerText=""
+});
+music.pause();
+turn="x";
+over=false
+document.getElementsByClassName("info")[0].innerText="Turn for "+turn;
+document.querySelector('.img-box').getElementsByTagName('img')[0].style.width="0px"
+
+   
+})
